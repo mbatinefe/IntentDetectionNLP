a183842603cd0e9eb0381b010fb13941e6f9916c
Mustafa Batın <119681291+mbatinefe@users.noreply.github.com>
2024-12-30 22:14:54 +0300

    Change Base

--- scripts/1.mt.prep.py
+++
@@ -1,7 +0,0 @@
-import myutils
-
-for lang in myutils.getMtLangs():
-    cmd = './scripts/1.mt.prep.sh ' + lang
-    print(cmd)
-
-
--- scripts/genAll.sh
+++
@@ -1,8 +0,0 @@
-python3 scripts/3.table.py
-
-python3 scripts/5.analysis.f1s.py
-python3 scripts/5.analysis.costs.py
-python3 scripts/5.analysis.langDist.py
-
-python3 scripts/6.appendix.auxData.py
-python3 scripts/6.appendix.auxScores.py
